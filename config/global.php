b659097cc672fca4d0ee63b03fda713536105ffb
bukers <bukers12gmail.com>
2019-01-09 11:48:17 -0600

    estrutura del proyecto
    es la extrutura del proyecto nuevo llamado sistema

---
+++ config/global.php
@@ -0,0 +1,14 @@
+<?php
+// Ip de la ppc de servidor de base de datos
+define("DB_HOST", "localhost");
+//nombre de la base de datos
+define("DB_NAME", "dbsistema");
+//usuario de l abse de datos
+define("DB_USERNAME", "root");
+//contraseña de la base de datos
+define("DB_PASSWORD", "");
+//se define la codificasion de los caracteres
+define("DB_ENCODE", "utf8");
+//definimos una constante como nombre del proyecto
+define("PRO_NOMBRE", "ITVentas");
+?>
